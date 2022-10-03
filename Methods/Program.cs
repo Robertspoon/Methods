39f1b4a222a91b74105eb7b6e1b928fb5e089932
Robertspoon <robertthemcjug@gmail.com>
2022-10-03 11:49:28 -0300

    yup

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,8 +8,40 @@ namespace Methods
 {
     internal class Program
     {
+          static int score;
+        static int Health;
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods");
+
+            score = 0;
+            int EnemyValue;
+            int EnemyDMG;
+            EnemyValue = 200;
+            Health = 100;
+            EnemyDMG = -50;
+
+
+            ShowHUD();
+            score = score + EnemyValue;
+            Health = Health + EnemyDMG;
+            ShowHUD();
+            
+
+            Console.ReadKey(true);
+
         }
-    }
+
+        
+
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("----------------");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + Health);
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+        }
+    } 
 }
